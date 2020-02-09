6689765341bcd1ce71dbb580e91ef0369a641dcc
Hsin Chang <HsinChang@users.noreply.github.com>
2020-02-09 20:24:23 +0100

    SD201
    Overall submit from local

---
+++ SD206/simpsons.pl
@@ -0,0 +1,62 @@
+parent(marge, lisa).
+
+parent(marge, bart).
+
+parent(marge, maggie).
+
+parent(homer, lisa).
+
+parent(homer, bart).
+
+parent(homer, maggie).
+
+parent(abraham, homer).
+
+parent(abraham, herb).
+
+parent(mona, homer).
+
+parent(jackie, marge).
+
+parent(clancy, marge).
+
+parent(jackie, patty).
+
+parent(clancy, patty).
+
+parent(jackie, selma).
+
+parent(clancy, selma).
+
+parent(selma, ling).
+
+
+
+female(mona).
+
+female(jackie).
+
+female(marge).
+
+female(ann).
+
+female(patty).
+
+female(selma).
+
+female(ling).
+
+female(lisa).
+
+female(maggie).
+
+male(abraham).
+
+male(herb).
+
+male(homer).
+
+male(bart).
+
+male(clancy).
+
